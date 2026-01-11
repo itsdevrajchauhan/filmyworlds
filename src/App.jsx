1ededc4582e1f4c51bf57c3ecdfa8c2fcf04f03b
itsdevrajchauhan <rajchauhan6279@gmail.com>
2026-01-11 14:15:23 +0530

    movielist Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import MovieList from "./components/MovieList/MovieList";
 
 const App =() =>{
   return(
     <div className ='app'>
    <Navbar/>
     <main> Main Contnet</main>
+    <MovieList/>
 
     </div>
   )
---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import './MovieList.css';
+import Fire from '../../assets/fire.png'
+
+const MovieList =()=>{
+  return(
+
+    <section className="movie_list">
+     <header className="align_center movie_list_header">
+     <h2 className="align_center movie_list_heading">Populor
+<img src={Fire} alt="fire emoji" className="navbar_emoji"/>
+</h2>
+   <div className=" align_center movie_list_fs">
+  <ul className="align_center movie_filter">
+   <li className="movie_filter_item active">8+ Star</li>
+   <li className="movie_filter_item"> 7+ Star</li>
+   <li className="movie_filter_item">6+ Star </li>
+  </ul>
+  
+        <select name= " " id=" " className="movie_sorting">
+        <option value="">Sortby</option>
+        <option value="">Date</option>
+        <option value="">Rating</option>
+        </select>
+
+        <select name= " " id=" " className="movie_sorting">
+        <option value="">assending</option>
+        <option value="">dessending</option>
+        </select>
+
+   </div>
+     </header>
+
+     <div className="movie_cards">
+     
+     </div>
+
+    </section>
+  )
+}
+
+export default MovieList;
